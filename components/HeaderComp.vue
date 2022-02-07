48fb2d05fb7bc898b93880d256b935d38be2f28b
Richard Tuttle <richard.tuttle@22squared.com>
2022-02-07 11:38:51 -0500

    gitignore updated

--- components/HeaderComp.vue
+++ components/HeaderComp.vue
@@ -1,10 +1,9 @@
 <template>
 	<header class="site-header">
 		<p v-if="$store.state.menu === 'Please create a menu document'" class="logo">{{ $store.state.menu }}</p>
-		<nuxt-link class="logo" to="/">{{ $prismic.asText($store.state.menu.title) }}</nuxt-link>
 		<nav>
 			<ul>
-				<li v-for="menuLink in $store.state.menu.menu_links" :key="menuLink.id">
+				<li v-for="menuLink in $store.state.menu.menu-links" :key="menuLink.id">
 					<prismic-link :field="menuLink.link">{{ $prismic.asText(menuLink.label) }}</prismic-link>
 				</li>
 			</ul>
@@ -19,8 +18,11 @@ export default {
 			document: {}
 		}
 	},
-	async fetch() {
-		this.document = await this.$prismic.api.getSingle('header');
+	// async fetch() {
+	// 	this.document = await this.$prismic.api.getSingle('header');
+	// },
+	async middleware({ store, $prismic }) {
+		await store.dispatch('fetchmenu', $prismic);
 	}
 }
 </script>
