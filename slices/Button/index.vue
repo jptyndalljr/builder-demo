7a53529ab569bb9b2e86bfa6a450b2b61a453b50
jptyndalljr <39528372+jptyndalljr@users.noreply.github.com>
2022-02-10 11:30:37 -0500

    Button slice (#2)

---
+++ slices/Button/index.vue
@@ -0,0 +1,39 @@
+<template>
+	<section class="section m-4">
+		<prismic-link class="button px-4 py-2" :field="slice.primary.link">{{
+			$prismic.asText(slice.primary.text)
+		}}</prismic-link>
+	</section>
+</template>
+
+<script>
+export default {
+	name: 'Button',
+	props: {
+		slice: {
+			type: Object,
+			required: true,
+			default() {
+				return {};
+			},
+		},
+	},
+};
+</script>
+
+<style scoped lang="scss">
+.section {
+	position: relative;
+}
+
+.button {
+	background: $button-bg;
+	border: 2px solid $button-outline;
+	font-family: $ff-sans;
+	font-weight: 700;
+	text-align: center;
+}
+a {
+	color: $button-text;
+}
+</style>
