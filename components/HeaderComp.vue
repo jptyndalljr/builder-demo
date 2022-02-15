dddd8679460b189220179d536f7693f73840b0b8
Richard Tuttle <richard.tuttle@22squared.com>
2022-02-15 11:23:40 -0500

    header

--- components/HeaderComp.vue
+++ components/HeaderComp.vue
@@ -1,13 +1,20 @@
 <template>
 	<header class="site-header">
-		<nav class="flex flex-row justify-center nav">
-			<img :src="doc.logo.url" :alt="doc.logo.alt" />
-			<ul class="sm:hidden md:flex md:flex-row md:grow md:justify-between">
-				<li v-for="(item, index) in this.menulist" :key="index" class="space-between p-5 nav-link">
-					<prismic-link :field="item.link" :target="item.target === true ? '_blank' : '_self'"><span class="main-nav-link">{{ item.label }}</span></prismic-link>
-				</li>
+		<nav class="xs:hidden">
+			<ul class="md:flex md:flex-row md:grow md:justify-between md:space-between md:items-center">
+				<span v-for="(item, index) in this.menulist" :key="index" class="p-5">
+					<li class="nav-link">
+						<prismic-link :field="item.link" :target="item.target === true ? '_blank' : '_self'"><span class="main-nav-link">{{ item.label }}</span></prismic-link>
+					</li>
+					<li v-if="index == list">
+						<nuxt-link to="/"><img :src="doc.logo.url" :alt="doc.logo.alt" class="nav-logo" /></nuxt-link>
+					</li>
+				</span>
 			</ul>
-			<font-awesome-icon class="md:hidden" :icon="['fas', 'bars']" size="2x" />
+		</nav>
+		<nav class="md:hidden">
+			<nuxt-link to="/"><img :src="doc.logo.url" :alt="doc.logo.alt" class="nav-logo" /></nuxt-link>
+			<font-awesome-icon :icon="['fas', 'bars']" size="2x" />
 		</nav>
 	</header>
 </template>
@@ -31,16 +38,22 @@ export default {
 		if (nav) {
 			this.menulist = nav.data['menu-links'];
 		}
+	},
+	computed: {
+		list() {
+			let ml = this.menulist.length;
+			return (Math.ceil(ml / 2)) - 1;
+		}
 	}
 }
 </script>
 
 <style lang="scss" scoped>
-.nav {
-	height: 75px;
-}
-
 .main-nav-link {
 	font-weight: bold;
+}
+
+.nav-logo {
+	width: 60px;
 }
 </style>
