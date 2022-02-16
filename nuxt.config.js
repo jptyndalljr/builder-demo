7bfaab95ce62f8d7641a18a269e9320e52f2701e
Richard Tuttle <richard.tuttle@22squared.com>
2022-02-16 10:46:47 -0500

    config updated

--- nuxt.config.js
+++ nuxt.config.js
@@ -46,9 +46,10 @@ export default {
     }, {
       rel: 'stylesheet',
       href: 'https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,400;1,500;1,600;1,700;1,800;1,900&family=Work+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap'
-    }, {
-		script: [{ src: "https://static.cdn.prismic.io/prismic.js?new=true&repo=22sq", async: true, defer: true }]
-	}]
+    }],
+	// script: [
+	// 	{ src: "https://static.cdn.prismic.io/prismic.js?new=true&repo=22sq", async: true, defer: true }
+	// ]
   },
   css: ['@/assets/scss/main.scss'],
   styleResources: {
