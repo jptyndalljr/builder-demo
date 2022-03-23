15ada4703e3fb6be5822686c9250c0fd26f489ab
Richard Tuttle <richard.tuttle@22squared.com>
2022-03-23 11:56:46 -0400

    tidy config

--- nuxt.config.js
+++ nuxt.config.js
@@ -32,23 +32,11 @@ export default {
       content: process.env.baseUrl
     }],
     link: [
-	// {
-    //   rel: 'preconnect',
-    //   href: 'https://fonts.googleapis.com'
-    // }, {
-    //   rel: 'preconnect',
-    //   href: 'https://fonts.gstatic.com',
-    //   crossorigin: 'true'
-    // },
 	{
       rel: 'icon',
       type: 'image/x-icon',
       href: '/favicon.ico'
     }],
-	// {
-    //   rel: 'stylesheet',
-    //   href: 'https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,400;1,500;1,600;1,700;1,800;1,900&family=Work+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap'
-    // }],
 	script: [
      	{ src: "https://static.cdn.prismic.io/prismic.js?new=true&repo=22sq", async: true, defer: true }
     ]
@@ -88,7 +76,7 @@ export default {
     trailingSlash: true
   },
   gtm: {
-    id: "123456789",
+    id: "xxx",
     pageTracking: true
   },
   fontawesome: {
